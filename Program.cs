60f5cdf36f0e8a653ee8acaf291eba1530efe84b
Denis Romazin <romazin777@gmail.com>
2022-09-27 20:26:54 +0500

    Задача 29

--- Program.cs
+++ Program.cs
@@ -20,17 +20,34 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.WriteLine("Введите число: ");
-int N = int.Parse(Console.ReadLine()!);
-Console.WriteLine($" {N} -> {GetSum(N)} ");
+// Console.WriteLine("Введите число: ");
+// int N = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($" {N} -> {GetSum(N)} ");
     
-int GetSum( int numb)
+// int GetSum( int numb)
+// {
+//     int sum = 0;
+//     while (numb > 0 )
+//     {
+//         sum = sum + (numb % 10);
+//         numb = numb / 10;
+//     }
+//     return sum;
+// }
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = massiv(8);    
+Console.WriteLine($"{String.Join(", ", array)} -> [{String.Join(", ", array)}]");
+
+int[] massiv(int size)
 {
-    int sum = 0;
-    while (numb > 0 )
+    int[] result = new int[size];
+    for (int i = 0 ; i < size ; i++)
     {
-        sum = sum + (numb % 10);
-        numb = numb / 10;
+        result[i] = new Random().Next(0, 50);
     }
-    return sum;
+    return result;
 }
